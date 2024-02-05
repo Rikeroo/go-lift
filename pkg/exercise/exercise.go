9cbc9cf5285096fb0ced51b7709f5b52f1656588
Rik Williamson <38403726+Rikeroo@users.noreply.github.com>
2024-02-05 12:31:12 +0000

    Fixed Exercise TestAddSet

--- pkg/exercise/exercise.go
+++ pkg/exercise/exercise.go
@@ -7,3 +7,7 @@ type Exercise struct {
 	Name string
 	Sets []set.Set
 }
+
+func (e *Exercise) AddSet(weight float32, reps int) {
+	// return [3]float32{0, 0, 0}
+}
--- pkg/exercise/exercise_test.go
+++ pkg/exercise/exercise_test.go
@@ -8,10 +8,19 @@ import (
 func TestAddSet(t *testing.T) {
 	var sets []set.Set
 	exercise := Exercise{Name: "Exercise test-name", Sets: sets}
-	weight := 10.0
+	var weight float32 = 10.0
 	reps := 8
 	exercise.AddSet(weight, reps)
 
 	want := [3]float32{1, 10.0, 8}
-	got := exercise.Sets[0].Get()
+
+	got := [3]float32{0, 0, 0}
+
+	if len(exercise.Sets) > 0 {
+		got = exercise.Sets[0].Get()
+	}
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
