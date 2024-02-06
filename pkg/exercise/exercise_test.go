b32f3c48d1272e471d23f35f8e9f161a1f3250a6
Rik Williamson <38403726+Rikeroo@users.noreply.github.com>
2024-02-06 13:47:16 +0000

    Implemented GetSetArray() function with tests

--- pkg/exercise/exercise_test.go
+++ pkg/exercise/exercise_test.go
@@ -15,15 +15,66 @@ func TestAddSet(t *testing.T) {
 
 		want := [3]float32{1, 10.0, 8}
 
-		got := [3]float32{0, 0, 0}
+		got := [3]float32{}
 
 		if len(exercise.Sets) > 0 {
 			got = exercise.Sets[0].Get()
 		}
 
-		if got != want {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertArrays(t, got, want)
 	})
 
+}
+
+func TestDeleteSet(t *testing.T) {
+	t.Run("Delete Set from Sets slice with length = 1", func(t *testing.T) {
+		exercise := makeExercise(t, 1)
+
+		setNum := 1
+		DelErr := exercise.DeleteSet(setNum)
+
+		want := [3]float32{}
+		got, GetErr := exercise.GetSetArray(setNum)
+
+		assertError(t, DelErr, nil)
+		assertError(t, GetErr, ErrNoSets)
+		assertArrays(t, got, want)
+	})
+
+}
+
+func TestGetSetArray(t *testing.T) {
+	exercise := makeExercise(t, 1)
+
+	want := [3]float32{1, 10, 8}
+	got, err := exercise.GetSetArray(1)
+
+	assertError(t, err, nil)
+	assertArrays(t, got, want)
+}
+
+func assertArrays(t testing.TB, got, want [3]float32) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func makeExercise(t testing.TB, setNums int) Exercise {
+	sets := []set.Set{}
+
+	for i := 0; i < setNums; i++ {
+		testSet := set.Set{SetNumber: i + 1, Weight: 10.0, Reps: 8}
+		sets = append(sets, testSet)
+	}
+	return Exercise{Name: "Exercise test-name", Sets: sets}
 }
