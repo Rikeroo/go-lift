f03bd97139b80073aa50f811c1677675f7946543
Rik Williamson <38403726+Rikeroo@users.noreply.github.com>
2024-02-08 11:43:22 +0000

    Server serves bare HTML workout detail using index.html template

--- src/main.go
+++ src/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"go-lift/pkg/workout"
+	"html/template"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -16,6 +20,36 @@ func main() {
 	w1.AddExercise(exerciseName)
 
 	// Add set to exercise
-	(w1.Exercises[exerciseName]).AddSet(10, 8)
+	w1.Exercises[exerciseName].AddSet(10, 8)
+	w1.Exercises[exerciseName].AddSet(12, 8)
+	w1.Exercises[exerciseName].AddSet(14, 6)
 
+	// Adding another exercise
+	exerciseName = "Squat"
+	w1.AddExercise(exerciseName)
+
+	// Add Sets to exercise
+	w1.Exercises[exerciseName].AddSet(30, 8)
+	w1.Exercises[exerciseName].AddSet(50, 8)
+	w1.Exercises[exerciseName].AddSet(70, 8)
+
+	// define variable for Exercise object
+	// e1 := w1.Exercises[exerciseName]
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, w1)
+	}
+	http.HandleFunc("/", h1)
+
+	startServer()
 }
+
+func startServer() {
+	fmt.Println("Starting Server...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+// func workoutHandler (workout workout.Workout) {
+// 	// Takes workout object
+// }
